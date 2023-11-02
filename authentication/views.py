b4a7e617ffa0346d7bfabdb870b8d8a3d7b08e5c
Hiroki Matsuuchi <hmatsuuchi@gmail.com>
2023-11-02 12:36:52 +0900

    tweaked authentication endpoints

--- authentication/views.py
+++ authentication/views.py
@@ -7,11 +7,13 @@ from rest_framework.views import APIView
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated, )
     
-    def post(self, request):
+    def post(self, request, format=None):
         try:
+            print("logout successful")
             refresh_token = request.data["refresh_token"]
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
+            print("logout error")
             return Response(status=status.HTTP_400_BAD_REQUEST)
