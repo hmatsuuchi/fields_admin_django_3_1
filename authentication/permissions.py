31759122d4aaa307d83b8b46234a7e48d6c6a730
Hiroki Matsuuchi <hmatsuuchi@gmail.com>
2024-01-15 10:35:30 +0900

    more test for student profiles

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -4,7 +4,7 @@ class isInStaffGroup(permissions.BasePermission):
     def has_permission(self, request, view):
         # checks if user is authenticated
         if not request.user.is_authenticated:
-            print("user not authenticated")
+            print("Error: user not authenticated")
             return False
         # checks if user in in Staff group
         return request.user.groups.filter(name="Staff").exists()
--- students/tests.py
+++ students/tests.py
@@ -4,8 +4,10 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
 from rest_framework_simplejwt.tokens import RefreshToken
-
 import json
+from datetime import date
+
+from .models import Students, Phone, PhoneChoice, PrefectureChoices, GradeChoices, StatusChoices, PaymentChoices
 
 # ======= Student Profiles List View Tests =======
 
@@ -69,10 +71,70 @@ class ProfilesListViewAsStaffGroupTest(TestCase):
 
 # ======= Student Profiles Details View Tests =======
         
-# users NOT logged in CANNOT access the student details view
+# users NOT logged in CANNOT access the student details view using GET, POST, PUT or DELETE
 class ProfilesDetailsViewAsUnauthenticatedUserTest(TestCase):
     def setUp(self):
         self.client = APIClient()
+
+        # create test prefecture choice
+        test_prefecture_choice = PrefectureChoices()
+        test_prefecture_choice.name = 'test_prefecture_choice'
+        test_prefecture_choice.order = 1
+        test_prefecture_choice.save()  
+        # create test phone choice
+        test_phone_choice = PhoneChoice()
+        test_phone_choice.name = 'test_phone_choice'
+        test_phone_choice.order = 1
+        test_phone_choice.save()
+        # create test phone
+        test_phone = Phone()
+        test_phone.number = 'test_phone_number'
+        test_phone.number_type = test_phone_choice
+        test_phone.save()
+        # create test grade choice
+        test_grade_choice = GradeChoices()
+        test_grade_choice.name = 'test_grade_choice'
+        test_grade_choice.order = 1
+        test_grade_choice.save()
+        # create test status choice
+        test_status_choice = StatusChoices()
+        test_status_choice.name = 'test_status_choice'
+        test_status_choice.order = 1
+        test_status_choice.save()
+        # create test payment choice
+        test_payment_choice = PaymentChoices()
+        test_payment_choice.name = 'test_payment_choice'
+        test_payment_choice.order = 1
+        test_payment_choice.save()
+        # creates test profile
+        test_profile = Students()
+        test_profile.save()
+        test_profile.last_name_romaji = 'last_name_romaji'
+        test_profile.first_name_romaji = 'first_name_romaji'
+        test_profile.last_name_kanji = 'last_name_kanji'
+        test_profile.first_name_kanji = 'first_name_kanji'
+        test_profile.last_name_katakana = 'last_name_katakana'
+        test_profile.first_name_katakana = 'first_name_katakana'
+        test_profile.post_code = 'post_code'
+        test_profile.prefecture = test_prefecture_choice
+        test_profile.city = 'city'
+        test_profile.address_1 = 'address_1'
+        test_profile.address_2 = 'address_2'
+        test_profile.phone.add(test_phone)
+        test_profile.birthday = date.today()
+        test_profile.grade = test_grade_choice
+        test_profile.status = test_status_choice
+        test_profile.payment_method = test_payment_choice
+        test_profile.archived = False
+        test_profile.save()
+        self.test_profile = test_profile
+
+    # GET - retrieve a student profile
+    def test_profiles_details_view_get(self):
+        params = {'profile_id': self.test_profile.id}
+
+        response = self.client.get(reverse('student_profiles_details'), params)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     # POST - create a new student profile
     def test_profiles_details_view_post(self):
@@ -100,8 +162,8 @@ class ProfilesDetailsViewAsUnauthenticatedUserTest(TestCase):
                     
         response = self.client.post(reverse('student_profiles_details'), json_data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        
-# users NOT in any group CANNOT access the student details view
+      
+# users NOT in any group CANNOT access the student details view using GET, POST, PUT or DELETE
 class ProfilesDetailsViewAsNoGroupTest(TestCase):
     def setUp(self):
         self.client = APIClient()
@@ -112,6 +174,66 @@ class ProfilesDetailsViewAsNoGroupTest(TestCase):
         self.access_token = str(refresh.access_token)
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
 
+        # create test prefecture choice
+        test_prefecture_choice = PrefectureChoices()
+        test_prefecture_choice.name = 'test_prefecture_choice'
+        test_prefecture_choice.order = 1
+        test_prefecture_choice.save()  
+        # create test phone choice
+        test_phone_choice = PhoneChoice()
+        test_phone_choice.name = 'test_phone_choice'
+        test_phone_choice.order = 1
+        test_phone_choice.save()
+        # create test phone
+        test_phone = Phone()
+        test_phone.number = 'test_phone_number'
+        test_phone.number_type = test_phone_choice
+        test_phone.save()
+        # create test grade choice
+        test_grade_choice = GradeChoices()
+        test_grade_choice.name = 'test_grade_choice'
+        test_grade_choice.order = 1
+        test_grade_choice.save()
+        # create test status choice
+        test_status_choice = StatusChoices()
+        test_status_choice.name = 'test_status_choice'
+        test_status_choice.order = 1
+        test_status_choice.save()
+        # create test payment choice
+        test_payment_choice = PaymentChoices()
+        test_payment_choice.name = 'test_payment_choice'
+        test_payment_choice.order = 1
+        test_payment_choice.save()
+        # creates test profile
+        test_profile = Students()
+        test_profile.save()
+        test_profile.last_name_romaji = 'last_name_romaji'
+        test_profile.first_name_romaji = 'first_name_romaji'
+        test_profile.last_name_kanji = 'last_name_kanji'
+        test_profile.first_name_kanji = 'first_name_kanji'
+        test_profile.last_name_katakana = 'last_name_katakana'
+        test_profile.first_name_katakana = 'first_name_katakana'
+        test_profile.post_code = 'post_code'
+        test_profile.prefecture = test_prefecture_choice
+        test_profile.city = 'city'
+        test_profile.address_1 = 'address_1'
+        test_profile.address_2 = 'address_2'
+        test_profile.phone.add(test_phone)
+        test_profile.birthday = date.today()
+        test_profile.grade = test_grade_choice
+        test_profile.status = test_status_choice
+        test_profile.payment_method = test_payment_choice
+        test_profile.archived = False
+        test_profile.save()
+        self.test_profile = test_profile
+
+    # GET - retrieve a student profile
+    def test_profiles_details_view_get(self):
+        params = {'profile_id': self.test_profile.id}
+
+        response = self.client.get(reverse('student_profiles_details'), params)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
     # POST - create a new student profile
     def test_profiles_details_view_post(self):
         data = {
@@ -139,7 +261,7 @@ class ProfilesDetailsViewAsNoGroupTest(TestCase):
         response = self.client.post(reverse('student_profiles_details'), json_data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-# users in the 'Customers' group CANNOT access the student details view
+# users in the 'Customers' group CANNOT access the student details view using GET, POST, PUT or DELETE
 class ProfilesDetailsViewAsCustomerGroupTest(TestCase):
     def setUp(self):
         self.client = APIClient()
@@ -152,6 +274,66 @@ class ProfilesDetailsViewAsCustomerGroupTest(TestCase):
         self.access_token = str(refresh.access_token)
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
 
+        # create test prefecture choice
+        test_prefecture_choice = PrefectureChoices()
+        test_prefecture_choice.name = 'test_prefecture_choice'
+        test_prefecture_choice.order = 1
+        test_prefecture_choice.save()  
+        # create test phone choice
+        test_phone_choice = PhoneChoice()
+        test_phone_choice.name = 'test_phone_choice'
+        test_phone_choice.order = 1
+        test_phone_choice.save()
+        # create test phone
+        test_phone = Phone()
+        test_phone.number = 'test_phone_number'
+        test_phone.number_type = test_phone_choice
+        test_phone.save()
+        # create test grade choice
+        test_grade_choice = GradeChoices()
+        test_grade_choice.name = 'test_grade_choice'
+        test_grade_choice.order = 1
+        test_grade_choice.save()
+        # create test status choice
+        test_status_choice = StatusChoices()
+        test_status_choice.name = 'test_status_choice'
+        test_status_choice.order = 1
+        test_status_choice.save()
+        # create test payment choice
+        test_payment_choice = PaymentChoices()
+        test_payment_choice.name = 'test_payment_choice'
+        test_payment_choice.order = 1
+        test_payment_choice.save()
+        # creates test profile
+        test_profile = Students()
+        test_profile.save()
+        test_profile.last_name_romaji = 'last_name_romaji'
+        test_profile.first_name_romaji = 'first_name_romaji'
+        test_profile.last_name_kanji = 'last_name_kanji'
+        test_profile.first_name_kanji = 'first_name_kanji'
+        test_profile.last_name_katakana = 'last_name_katakana'
+        test_profile.first_name_katakana = 'first_name_katakana'
+        test_profile.post_code = 'post_code'
+        test_profile.prefecture = test_prefecture_choice
+        test_profile.city = 'city'
+        test_profile.address_1 = 'address_1'
+        test_profile.address_2 = 'address_2'
+        test_profile.phone.add(test_phone)
+        test_profile.birthday = date.today()
+        test_profile.grade = test_grade_choice
+        test_profile.status = test_status_choice
+        test_profile.payment_method = test_payment_choice
+        test_profile.archived = False
+        test_profile.save()
+        self.test_profile = test_profile
+
+    # GET - retrieve a student profile
+    def test_profiles_details_view_get(self):
+        params = {'profile_id': self.test_profile.id}
+
+        response = self.client.get(reverse('student_profiles_details'), params)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
     # POST - create a new student profile
     def test_profiles_details_view_post(self):
         data = {
@@ -179,7 +361,7 @@ class ProfilesDetailsViewAsCustomerGroupTest(TestCase):
         response = self.client.post(reverse('student_profiles_details'), json_data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-# users in the 'Staff' group CAN access the student details view
+# users in the 'Staff' group CAN access the student details view using GET, POST, PUT or DELETE
 class ProfilesDetailsViewAsStaffGroupTest(TestCase):
     def setUp(self):
         self.client = APIClient()
@@ -191,6 +373,66 @@ class ProfilesDetailsViewAsStaffGroupTest(TestCase):
         refresh = RefreshToken.for_user(self.user)
         self.access_token = str(refresh.access_token)
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+
+        # create test prefecture choice
+        test_prefecture_choice = PrefectureChoices()
+        test_prefecture_choice.name = 'test_prefecture_choice'
+        test_prefecture_choice.order = 1
+        test_prefecture_choice.save()  
+        # create test phone choice
+        test_phone_choice = PhoneChoice()
+        test_phone_choice.name = 'test_phone_choice'
+        test_phone_choice.order = 1
+        test_phone_choice.save()
+        # create test phone
+        test_phone = Phone()
+        test_phone.number = 'test_phone_number'
+        test_phone.number_type = test_phone_choice
+        test_phone.save()
+        # create test grade choice
+        test_grade_choice = GradeChoices()
+        test_grade_choice.name = 'test_grade_choice'
+        test_grade_choice.order = 1
+        test_grade_choice.save()
+        # create test status choice
+        test_status_choice = StatusChoices()
+        test_status_choice.name = 'test_status_choice'
+        test_status_choice.order = 1
+        test_status_choice.save()
+        # create test payment choice
+        test_payment_choice = PaymentChoices()
+        test_payment_choice.name = 'test_payment_choice'
+        test_payment_choice.order = 1
+        test_payment_choice.save()
+        # creates test profile
+        test_profile = Students()
+        test_profile.save()
+        test_profile.last_name_romaji = 'last_name_romaji'
+        test_profile.first_name_romaji = 'first_name_romaji'
+        test_profile.last_name_kanji = 'last_name_kanji'
+        test_profile.first_name_kanji = 'first_name_kanji'
+        test_profile.last_name_katakana = 'last_name_katakana'
+        test_profile.first_name_katakana = 'first_name_katakana'
+        test_profile.post_code = 'post_code'
+        test_profile.prefecture = test_prefecture_choice
+        test_profile.city = 'city'
+        test_profile.address_1 = 'address_1'
+        test_profile.address_2 = 'address_2'
+        test_profile.phone.add(test_phone)
+        test_profile.birthday = date.today()
+        test_profile.grade = test_grade_choice
+        test_profile.status = test_status_choice
+        test_profile.payment_method = test_payment_choice
+        test_profile.archived = False
+        test_profile.save()
+        self.test_profile = test_profile
+
+    # GET - retrieve a student profile
+    def test_profiles_details_view_get(self):
+        params = {'profile_id': self.test_profile.id}
+
+        response = self.client.get(reverse('student_profiles_details'), params)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     # POST - create a new student profile
     def test_profiles_details_view_post(self):
