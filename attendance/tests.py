eb6f85d3e40b053dfafd3cf1a04274bd75b3865d
Hiroki Matsuuchi <hmatsuuchi@gmail.com>
2025-03-21 09:08:49 +0900

    additional tests and generated requirements.txt

--- attendance/tests.py
+++ attendance/tests.py
@@ -4,10 +4,13 @@ from django.contrib.auth.models import User, Group
 # drf imports
 from rest_framework import status
 from rest_framework.test import APIClient
+# json imports
+import json
 # model imports
 from .models import Attendance, AttendanceRecord, AttendanceRecordStatus
 from schedule.models import Events, EventType
-from students.models import Students, GradeChoices
+from students.models import Students, GradeChoices, StatusChoices
+from user_profiles.models import UserProfilesInstructors
 
 # ======= Attendance For Date View Tests =======
 
@@ -19,7 +22,7 @@ class AttendanceForDateViewAsUnauthenticatedUserTest(TestCase):
         # create test client
         self.client = APIClient()
 
-    def test_events_all_view(self):
+    def test_attendance_for_date_view_get(self):
         # attempt to access events all view
         response = self.client.get('/api/attendance/attendance/single_date/')
 
@@ -39,7 +42,7 @@ class AttendanceForDateViewAsNoGroupTest(TestCase):
         # authenticate user
         self.client.force_authenticate(user=self.user)
 
-    def test_events_all_view(self):
+    def test_attendance_for_date_view_get(self):
         # attempt to access events all view
         response = self.client.get('/api/attendance/attendance/single_date/')
 
@@ -63,7 +66,7 @@ class AttendanceForDateViewAsCustomersGroupTest(TestCase):
         # authenticate user
         self.client.force_authenticate(user=self.user)
 
-    def test_events_all_view(self):
+    def test_attendance_for_date_view_get(self):
         # attempt to access events all view
         response = self.client.get('/api/attendance/attendance/single_date/')
 
@@ -87,7 +90,7 @@ class AttendanceForDateViewAsStaffGroupTest(TestCase):
         # authenticate user
         self.client.force_authenticate(user=self.user)
 
-    def test_events_all_view(self):
+    def test_attendance_for_date_view_get(self):
         params = {
             'date': '2022-01-01',
             'instructor_id': self.user.id,
@@ -173,7 +176,7 @@ class AttendanceForDateViewAContentRetrievalTest(TestCase):
 
         attendance.attendance_records.add(attendance_record)
 
-    def test_events_all_view(self):
+    def test_attendance_for_date_view_get(self):
         params = {
             'date': '2022-01-01',
             'instructor_id': self.user.id,
@@ -215,9 +218,158 @@ class AttendanceDetailsViewAsUnauthenticatedUserTest(TestCase):
         # create test client
         self.client = APIClient()
 
-    def test_events_all_view(self):
+    def test_attendance_details_view_post(self):
         # attempt to access events all view
-        response = self.client.get('/api/attendance/attendance/attendance_details/')
+        response = self.client.post('/api/attendance/attendance/attendance_details/')
 
         # response status code is 401 UNAUTHORIZED
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+# users logged in but NOT in any group CANNOT access the attendance details view
+class AttendanceDetailsViewAsNoGroupTest(TestCase):
+    def setUp(self):
+        # create test client
+        self.client = APIClient()
+
+        # create user
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+
+        # authenticate user
+        self.client.force_authenticate(user=self.user)
+
+    def test_attendance_details_view_post(self):
+        # attempt to access events all view
+        response = self.client.post('/api/attendance/attendance/attendance_details/')
+
+        # response status code is 403 FORBIDDEN
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+# users looged in but in the 'Customers' group CANNOT access the attendance details view
+class AttendanceDetailsViewAsCustomersGroupTest(TestCase):
+    def setUp(self):
+        # create test client
+        self.client = APIClient()
+
+        # create user
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+
+        # add user to 'Customers' group
+        customers_group = Group.objects.create(name='Customers')
+        self.user.groups.add(customers_group)
+
+        # authenticate user
+        self.client.force_authenticate(user=self.user)
+
+    def test_attendance_details_view_post(self):
+        # attempt to access events all view
+        response = self.client.post('/api/attendance/attendance/attendance_details/')
+
+        # response status code is 403 FORBIDDEN
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+# users logged in and in the 'Staff' group CAN access the attendance details view
+class AttendanceDetailsViewAsStaffGroupTest(TestCase):
+    def setUp(self):
+        # create test client
+        self.client = APIClient()
+
+        # create user
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+
+        # add user to 'Staff' group
+        staff_group = Group.objects.create(name='Staff')
+        self.user.groups.add(staff_group)
+
+        # authenticate user
+        self.client.force_authenticate(user=self.user)
+
+        # create event type
+        self.event_type = EventType()
+        self.event_type.duration = 99
+        self.event_type.capacity = 9
+        self.event_type.save()
+
+        # create linked class
+        self.linked_class = Events()
+        self.linked_class.event_name = 'Test Event'
+        self.linked_class.event_type = self.event_type
+        self.linked_class.primary_instructor = self.user
+        self.linked_class.day_of_week = 1
+        self.linked_class.start_time = '01:01:01'
+        self.linked_class.save()
+
+        # create grade choice
+        self.grade_choice = GradeChoices()
+        self.grade_choice.name = 'Test Grade'
+        self.grade_choice.order = 99
+        self.grade_choice.save()
+
+        # create status choice
+        self.status_choice = StatusChoices()
+        self.status_choice.name = 'Test Status'
+        self.status_choice.order = 99
+        self.status_choice.save()
+
+        # create attendance record status
+        self.attendance_record_status = AttendanceRecordStatus()
+        self.attendance_record_status.status_name = 'Test Status'
+        self.attendance_record_status.save()
+
+        # create student
+        self.student = Students()
+        self.student.last_name_romaji = 'Test Student Last'
+        self.student.first_name_romaji = 'Test Student First'
+        self.student.last_name_kanji = '試験'
+        self.student.first_name_kanji = '学生'
+        self.student.last_name_katakana = 'テスト'
+        self.student.first_name_katakana = 'ガクセイ'
+        self.student.grade = self.grade_choice
+        self.student.status = self.status_choice
+        self.student.save()
+
+        # create attendance record
+        self.attendance_record = AttendanceRecord()
+        self.attendance_record.student = self.student
+        self.attendance_record.status = self.attendance_record_status
+        self.attendance_record.grade = self.grade_choice
+        self.attendance_record.save()
+
+        # create user profile instructor
+        self.user_profile_instructor = UserProfilesInstructors()
+        self.user_profile_instructor.user = self.user
+        self.user_profile_instructor.last_name_romaji = 'Test Instructor Last'
+        self.user_profile_instructor.first_name_romaji = 'Test Instructor First'
+        self.user_profile_instructor.icon_stub = '/media/test_instructor_icon.jpg'
+        self.user_profile_instructor.save()
+
+        # create attendance
+        self.attendance = Attendance()
+        self.attendance.linked_class = self.linked_class
+        self.attendance.instructor = self.user
+        self.attendance.date = '2022-01-01'
+        self.attendance.start_time = '01:01:01'
+        self.attendance.save()
+
+    def test_attendance_details_view_post(self):
+        json_data = {
+            'linked_class': self.linked_class.id,
+            'instructor': self.user.id,
+            'date': '2022-01-01',
+            'start_time': '01:01:01',
+            'attendance_records':
+            [{
+                    'student': self.student.id,
+                    'status': self.status_choice.id,
+                    'grade': self.grade_choice.id,
+                },
+            ],
+        }
+
+        # attempt to access events all view
+        response = self.client.post('/api/attendance/attendance/attendance_details/', json.dumps(json_data), content_type='application/json')
+
+        # response status code is 201 CREATED
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
